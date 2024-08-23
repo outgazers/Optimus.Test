531b21802c30c72630246b940f9d33d8ce6247f7
outgazers <mortezaabareshi1994@gmail.com>
2024-08-23 13:49:46 +0330

    add migration

---
+++ customers/src/Optimus.Services.Customers.Infrastructure/Postgres/Migrations/20240823101913_update-modes-of-transportation.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Optimus.Services.Customers.Infrastructure.Migrations
+{
+    public partial class updatemodesoftransportation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ModsOfTransportation",
+                schema: "customers",
+                table: "Customers",
+                newName: "ModesOfTransportation");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ModesOfTransportation",
+                schema: "customers",
+                table: "Customers",
+                newName: "ModsOfTransportation");
+        }
+    }
+}
