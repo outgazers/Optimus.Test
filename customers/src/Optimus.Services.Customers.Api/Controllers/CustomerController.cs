e56c3abdfe082e3b6a649de0d3bb49390080a647
outgazers <mortezaabareshi1994@gmail.com>
2024-08-28 20:43:09 +0330

    review changes

--- customers/src/Optimus.Services.Customers.Api/Controllers/CustomerController.cs
+++ customers/src/Optimus.Services.Customers.Api/Controllers/CustomerController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Optimus.Services.Customers.Application.Events;
 using Optimus.Services.Customers.Application.Services;
 using Optimus.Services.Customers.Core.Entities;
+using Optimus.Services.Customers.Core.Events;
 using SharedAbstractions.Queries;
 using Swashbuckle.AspNetCore.Annotations;
 
@@ -18,24 +19,56 @@ public class CustomerController : ControllerBase
     private readonly ICommandDispatcher _commandDispatcher;
     private readonly IQueryDispatcher _queryDispatcher;
     private readonly IMessageBroker _messageBroker;
+    private readonly IEventMapper _eventMapper;
 
-    public CustomerController(ICommandDispatcher commandDispatcher, IQueryDispatcher queryDispatcher, IMessageBroker messageBroker)
+    public CustomerController(ICommandDispatcher commandDispatcher, IQueryDispatcher queryDispatcher, IMessageBroker messageBroker, IEventMapper eventMapper)
     {
         _commandDispatcher = commandDispatcher;
         _queryDispatcher = queryDispatcher;
         _messageBroker = messageBroker;
+        _eventMapper = eventMapper;
     }
     [HttpGet("/test")]
     public async Task<IActionResult> Test()
     {
+
         var modesOfTransportation = new List<ModesOfTransportation>
         {
-            ModesOfTransportation.FTL
+            ModesOfTransportation.AirFreight,
+            ModesOfTransportation.FTL,
+            ModesOfTransportation.CrossBorder,
+            ModesOfTransportation.OceanFreight
         };
         var address = "Los Angles";
         var industry = "Vitamins";
-        var customerEvent = new CustomerCreated(new Guid("2adc0434-5f2c-4fd3-ac6e-9bd445855d0b"), modesOfTransportation, industry, address);
-        await _messageBroker.PublishAsync(customerEvent);
+
+        var customer = new Customer(
+            Guid.NewGuid(),
+            "optimus@gmail.com",
+            DateTime.UtcNow,
+            DateTime.UtcNow,
+            "optimusUser",
+            1
+        );
+        
+        customer.CompleteRegistrationFromUser(
+            "Optimus Prime",
+            "Los Angles",
+            "Autobots Inc.",
+            "123456",
+            "123-456-7890",
+            "30 days",
+            "OptimusTMS",
+            true,
+            modesOfTransportation,
+            industry,
+            10
+        ); 
+        var e = _eventMapper.MapAll(customer.Events);
+        if(e.Any())
+            await _messageBroker.PublishAsync(e.ToArray());
+        // var customerEvent = new CustomerCreated(new Guid("2adc0434-5f2c-4fd3-ac6e-9bd445855d0b"), modesOfTransportation, industry, address);
+        // await _messageBroker.PublishAsync(customerEvent);
         return Ok("Test");
     }
 
